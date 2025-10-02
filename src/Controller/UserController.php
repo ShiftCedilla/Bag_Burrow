a3a4f33dda82f46d9025d66cf61aef6e9abfe261
AIRASSART64 <irassarta@gmail.com>
2025-10-02 13:45:20 +0200

    'usermanagemenr1'

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,11 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index()
     {
-        
+        $user = $this->getUser();        
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user'=>$user,
         ]);
     }
 }
+
+//  
